641a519f5aa7aff193099eb8a607a6fddeb898dd
ayrton-hbtn <3282@holbertonschool.com>
2022-02-26 23:46:47 -0300

    filters on feed

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,26 @@
 $(document).ready(function () {
     $('#lost').click(function () {
-        $('#found').toggleClass('btn-outline-success btn-success');
+        $('div.pet_found').each(function(i, obj) {
+            $(obj).hide();
+        });
+        $('div.pet_lost').each(function(i, obj) {
+            $(obj).show();
+        });
+    });
+    $('#found').click(function () {
+        $('div.pet_lost').each(function(i, obj) {
+            $(obj).hide();
+        });
+        $('div.pet_found').each(function(i, obj) {
+            $(obj).show();
+        });
+    });
+    $('#all').click(function () {
+        $('div.pet_lost').each(function(i, obj) {
+            $(obj).show();
+        });
+        $('div.pet_found').each(function(i, obj) {
+            $(obj).show();
+        });
     });
 });
