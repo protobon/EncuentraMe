8a8744c17eba6bf99de5b0a2f6c96904f94ff96a
Andy <3283@holbertonschool.com>
2022-03-02 18:56:44 -0300

    Added table for report

---
+++ mysql/create_table_report.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS reports(
+    id VARCHAR(41) PRIMARY KEY,
+    created_at DATETIME NOT NULL,
+    descripcion VARCHAR(512) NOT NULL,
+    post_id VARCHAR(41) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+);
