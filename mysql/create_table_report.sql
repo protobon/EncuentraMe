8da04e912ebe93ba6ee5bdafc9f57926203796e3
ayrton-hbtn <3282@holbertonschool.com>
2022-03-02 19:10:41 -0300

    table reports

--- mysql/create_table_report.sql
+++ mysql/create_table_report.sql
@@ -1,7 +1,8 @@
-CREATE TABLE IF NOT EXISTS reports(
+CREATE TABLE IF NOT EXISTS reports (
     id VARCHAR(41) PRIMARY KEY,
     created_at DATETIME NOT NULL,
     descripcion VARCHAR(512) NOT NULL,
     post_id VARCHAR(41) NOT NULL,
+    user_id VARCHAR(20) NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(id),
 );
