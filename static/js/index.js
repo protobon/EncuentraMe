74e354a63d7b66524e0e7ea2d5f8145a5e2e2ed2
ayrton-hbtn <3282@holbertonschool.com>
2022-03-02 01:48:17 -0300

    style buttons

--- static/js/index.js
+++ static/js/index.js
@@ -30,4 +30,28 @@ $(document).ready(function () {
             $(obj).show();
         });
     });
+    $('#all').click(function() {
+        $('#all').addClass("btn-success");
+        $('#all').removeClass("btn-outline-success");
+        $('#lost').addClass("btn-outline-success");
+        $('#lost').removeClass("btn-success");
+        $('#found').addClass("btn-outline-success");
+        $('#found').removeClass("btn-success");
+    });
+    $('#lost').click(function() {
+        $('#lost').addClass("btn-success");
+        $('#lost').removeClass("btn-outline-success");
+        $('#all').addClass("btn-outline-success");
+        $('#all').removeClass("btn-success");
+        $('#found').addClass("btn-outline-success");
+        $('#found').removeClass("btn-success");
+    });
+    $('#found').click(function() {
+        $('#found').addClass("btn-success");
+        $('#found').removeClass("btn-outline-success");
+        $('#all').addClass("btn-outline-success");
+        $('#all').removeClass("btn-success");
+        $('#lost').addClass("btn-outline-success");
+        $('#lost').removeClass("btn-success");
+    });
 });
