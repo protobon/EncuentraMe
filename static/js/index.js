15c0af7d2929192c69634392daae7887ca2897eb
ayrton-hbtn <3282@holbertonschool.com>
2022-03-02 01:22:37 -0300

    .

--- static/js/index.js
+++ static/js/index.js
@@ -1,27 +1,27 @@
-$(document).ready(function () {
-    $('#lost').click(function () {
-        $('div.pet_found').each(function(i, obj) {
-            $(obj).hide();
-        });
-        $('div.pet_lost').each(function(i, obj) {
-            $(obj).show();
-        });
-    });
-    $('#found').click(function () {
-        $('div.pet_lost').each(function(i, obj) {
-            $(obj).hide();
-        });
-        $('div.pet_found').each(function(i, obj) {
-            $(obj).show();
-        });
-    });
-    $('#all').click(function () {
-        refreshFeed();
-        $('div.pet_lost').each(function(i, obj) {
-            $(obj).show();
-        });
-        $('div.pet_found').each(function(i, obj) {
-            $(obj).show();
-        });
-    });
-});
+// $(document).ready(function () {
+//     $('#lost').click(function () {
+//         $('div.pet_found').each(function(i, obj) {
+//             $(obj).hide();
+//         });
+//         $('div.pet_lost').each(function(i, obj) {
+//             $(obj).show();
+//         });
+//     });
+//     $('#found').click(function () {
+//         $('div.pet_lost').each(function(i, obj) {
+//             $(obj).hide();
+//         });
+//         $('div.pet_found').each(function(i, obj) {
+//             $(obj).show();
+//         });
+//     });
+//     $('#all').click(function () {
+//         refreshFeed();
+//         $('div.pet_lost').each(function(i, obj) {
+//             $(obj).show();
+//         });
+//         $('div.pet_found').each(function(i, obj) {
+//             $(obj).show();
+//         });
+//     });
+// });
