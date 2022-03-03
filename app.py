26f8380f8671b044e4ea924908a65591102ba08a
ayrton-hbtn <3282@holbertonschool.com>
2022-03-03 16:34:43 -0300

    debugging

--- app.py
+++ app.py
@@ -167,13 +167,15 @@ def api_users():
     else:
         """Stores new user into database"""
         content_type = request.headers.get('Content-Type')
+        flash(content_type)
         if (content_type != 'application/json'):
             return (jsonify("Not a JSON"), 400)
         user = request.get_json()
+        flash(user)
         try:
             cursor.execute('INSERT INTO users VALUES (%s, %s, %s)', (user['id'], user['name'], user['email']))
-        except Exception:
-            pass
+        except Exception as e:
+            flash(e)
         cursor.close()
         return jsonify(user)
 
