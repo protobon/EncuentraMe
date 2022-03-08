975d118239eb206b3aaef49171f0de4cf6542505
ralexrivero <ralexrivero@gmail.com>
2022-03-08 16:04:25 -0300

    clean files

--- _temporal/webstack/01-install_nginx.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-#install nginx and create index.html file with Hello World text
-
-sudo apt update
-sudo apt-get -y install nginx
-sudo service nginx restart
--- _temporal/webstack/06-install_apache_bench.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# install apache bench
-
-sudo apt-get update
-sudo apt-get install apache2-utils
