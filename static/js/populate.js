08c079d11c06c612bf3f5148a7f6651ccdc0ebc1
ralexrivero <ralexrivero@gmail.com>
2022-03-09 05:16:39 -0300

    populate

--- static/js/populate.js
+++ static/js/populate.js
@@ -13,25 +13,28 @@ $(document).ready(function () {
     }
     window.refreshFeed = function() {
         fetchAllPosts().then(function(data) {
-            $('article.posts').empty();
+            $('div.posts').empty();
             $('#todos').prop('checked', true);
             $.each(data.lost, function () {
                 this.created_at = new Date(this.created_at).toLocaleString('es-UY');
                 let postLostNew = $(document.createElement('div'));
+                postLostNew.addClass('col');
                 postLostNew.addClass('pet');
                 postLostNew.addClass('pet_lost');
                 postLostNew.addClass(this.mascota);
+                postLostNew.append('<div class="card shadow-sm"></div>');
+                postLostNew.append('<a href="/' + this.id + '"></a>');
+                postLostNew.find('a').append('<img src="/static/images/' + this.foto + '">');
                 let userInfo = $(document.createElement('div'));
                 userInfo.addClass('user');
-                userInfo.append('<h3>' + this.nombre +' perdido/a!</h3>');
-                userInfo.append('<p>Fecha de publicación: ' + this.created_at +'</p>');
+                userInfo.addClass('card-body');
+                userInfo.append('<h3>' + this.nombre + ' perdido/a!</h3>');
+                userInfo.append('<p class="card-text">Fecha de publicación: ' + this.created_at + '</p>');
                 postLostNew.append(userInfo);
                 postLostNew.append('<p>¡Se busca a ' + this.nombre + '! Perdido/a desde el día '
                 + this.fecha + ' última vez visto en las inmediaciones de ' + this.calle_1 +
                 ' y ' + this.calle_2 + ' barrio ' + this.barrio + ' a las ' + this.hora + ' horas.\n'
                 + 'Si lo viste por favor comunícate con ' + this.user_name + '.</p>');
-                postLostNew.append('<a href="/' + this.id + '"></a>');
-                postLostNew.find('a').append('<img src="/static/images/' + this.foto + '">');
                 let reportButton = $(document.createElement('button'));
                 reportButton.html('Denunciar publicación');
                 reportButton.addClass('btn');
@@ -39,7 +42,7 @@ $(document).ready(function () {
                 reportButton.addClass('btn-sm');
                 reportButton.attr('onclick', 'reportPost("' + this.id + '")');
                 postLostNew.append(reportButton);
-                $('article.posts').append(postLostNew);
+                $('div.posts').append(postLostNew);
             });
             $.each(data.found, function () {
                 this.created_at = new Date(this.created_at).toLocaleString('es-UY');
@@ -59,20 +62,21 @@ $(document).ready(function () {
                 postFoundNew.append('<a href="/' + this.id + '"></a>');
                 postFoundNew.find('a').append('<img src="/static/images/' + this.foto + '">');
                 let reportButton = $(document.createElement('button'));
+                reportButton.prop({ type: 'button' });
                 reportButton.html('Denunciar publicación');
                 reportButton.addClass('btn');
                 reportButton.addClass('btn-link');
                 reportButton.addClass('btn-sm');
                 reportButton.attr('onclick', 'reportPost("' + this.id + '")');
                 postFoundNew.append(reportButton);
-                $('article.posts').append(postFoundNew);
+                $('div.posts').append(postFoundNew);
             });
         });
     }
     refreshFeed();
     window.refreshLost = function() {
         fetchAllPosts().then(function(data) {
-            $('article.posts').empty();
+            $('div.posts').empty();
             $('#todos').prop('checked', true);
             $.each(data.lost, function () {
                 this.created_at = new Date(this.created_at).toLocaleString('es-UY');
@@ -99,13 +103,13 @@ $(document).ready(function () {
                 reportButton.addClass('btn-sm');
                 reportButton.attr('onclick', 'reportPost("' + this.id + '")');
                 postLostNew.append(reportButton);
-                $('article.posts').append(postLostNew);
+                $('div.posts').append(postLostNew);
             });
         });
     }
     window.refreshFound = function() {
         fetchAllPosts().then(function(data) {
-            $('article.posts').empty();
+            $('div.posts').empty();
             $('#todos').prop('checked', true);
             $.each(data.found, function () {
                 this.created_at = new Date(this.created_at).toLocaleString('es-UY');
@@ -132,7 +136,7 @@ $(document).ready(function () {
                 reportButton.addClass('btn-sm');
                 reportButton.attr('onclick', 'reportPost("' + this.id + '")');
                 postFoundNew.append(reportButton);
-                $('article.posts').append(postFoundNew);
+                $('div.posts').append(postFoundNew);
             });
         });
     }
