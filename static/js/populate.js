5c213cd67196653a0d8d70d610abb299da2cfcfd
ralexrivero <ralexrivero@gmail.com>
2022-03-09 03:25:37 -0300

    back

--- static/js/populate.js
+++ static/js/populate.js
@@ -1,41 +1,37 @@
 $(document).ready(function () {
-  async function fetchAllPosts() {
-      try {
-          const response = await fetch('https://encuentrame.org.xelar.tech/api/posts/');
-          if (!response.ok) {
-              throw new Error(`Error! status: ${response.status}`);
-          }
-          const data = await response.json();
-          return (data);
-      } catch (err) {
-          console.log(err);
-      }
-  }
-    window.refreshFeed = function () {
-        fetchAllPosts().then(function (data) {
+    async function fetchAllPosts() {
+        try {
+            const response = await fetch('https://encuentrame.org.xelar.tech/api/posts/');
+            if (!response.ok) {
+                throw new Error(`Error! status: ${response.status}`);
+            }
+            const data = await response.json();
+            return (data);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+    window.refreshFeed = function() {
+        fetchAllPosts().then(function(data) {
             $('div.posts').empty();
             $('#todos').prop('checked', true);
             $.each(data.lost, function () {
                 this.created_at = new Date(this.created_at).toLocaleString('es-UY');
-                let col = $(document.createElement('div'));
-                col.addClass('col');
                 let postLostNew = $(document.createElement('div'));
                 postLostNew.addClass('pet');
                 postLostNew.addClass('pet_lost');
                 postLostNew.addClass(this.mascota);
-                postLostNew.append('<a href="/' + this.id + '"></a>');
-                postLostNew.find('a').append('<img src="/static/images/' + this.foto + '">');
+                let userInfo = $(document.createElement('div'));
+                userInfo.addClass('user');
+                userInfo.append('<h3>' + this.nombre +' perdido/a!</h3>');
+                userInfo.append('<p>Fecha de publicación: ' + this.created_at +'</p>');
                 postLostNew.append(userInfo);
                 postLostNew.append('<p>¡Se busca a ' + this.nombre + '! Perdido/a desde el día '
                 + this.fecha + ' última vez visto en las inmediaciones de ' + this.calle_1 +
                 ' y ' + this.calle_2 + ' barrio ' + this.barrio + ' a las ' + this.hora + ' horas.\n'
                 + 'Si lo viste por favor comunícate con ' + this.user_name + '.</p>');
-
-                let userInfo = $(document.createElement('div'));
-                userInfo.addClass('user');
-                userInfo.append('<h3>' + this.nombre + ' perdido/a!</h3>');
-                userInfo.append('<p>Fecha de publicación: ' + this.created_at + '</p>');
-
+                postLostNew.append('<a href="/' + this.id + '"></a>');
+                postLostNew.find('a').append('<img src="/static/images/' + this.foto + '">');
                 let reportButton = $(document.createElement('button'));
                 reportButton.html('Denunciar publicación');
                 reportButton.addClass('btn');
@@ -45,7 +41,6 @@ $(document).ready(function () {
                 postLostNew.append(reportButton);
                 $('div.posts').append(postLostNew);
             });
-
             $.each(data.found, function () {
                 this.created_at = new Date(this.created_at).toLocaleString('es-UY');
                 let postFoundNew = $(document.createElement('div'));
@@ -54,8 +49,8 @@ $(document).ready(function () {
                 postFoundNew.addClass(this.mascota);
                 let userInfo = $(document.createElement('div'));
                 userInfo.addClass('user');
-                userInfo.append('<h3>' + this.mascota + ' encontrado/a!</h3>');
-                userInfo.append('<p>Fecha de publicación: ' + this.created_at + '</p>');
+                userInfo.append('<h3>' + this.mascota +' encontrado/a!</h3>');
+                userInfo.append('<p>Fecha de publicación: ' + this.created_at +'</p>');
                 postFoundNew.append(userInfo);
                 postFoundNew.append('<p>Se encontró el día ' + this.fecha + ' por barrio '
                 + this.barrio + ' en las inmediaciones de ' + this.calle_1 +
