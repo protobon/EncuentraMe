8e6dc3cb61090aff59517f1f990928fe34f22c4a
ayrton-hbtn <3282@holbertonschool.com>
2022-03-11 17:19:41 -0300

    debugging block user

--- app.py
+++ app.py
@@ -333,10 +333,12 @@ def api_users():
     if request.method == 'PUT':
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         user = request.get_json()
+        logfile(str(user))
+        logfile(str(type(user)))
         try:
             cursor.execute("UPDATE users SET estado='blocked' WHERE id=%s", [user['id']])
         except Exception as e:
-            logfile(str(e))
+            logfile("/api/users - UPDATE users:\n" + str(e))
         mysql.connection.commit()
         cursor.close()
         flash('Usuario bloqueado', "info")
