f53a5d3da3ff78ed48ca5b2847a7947cb7cd9112
ralexrivero <ralexrivero@gmail.com>
2022-03-10 23:51:58 -0300

    general fixes

--- static/js/populate.js
+++ static/js/populate.js
@@ -13,28 +13,25 @@ $(document).ready(function () {
     }
     window.refreshFeed = function() {
         fetchAllPosts().then(function(data) {
-            $('div.posts').empty();
+            $('article.posts').empty();
             $('#todos').prop('checked', true);
             $.each(data.lost, function () {
                 this.created_at = new Date(this.created_at).toLocaleString('es-UY');
                 let postLostNew = $(document.createElement('div'));
-                postLostNew.addClass('col');
                 postLostNew.addClass('pet');
                 postLostNew.addClass('pet_lost');
                 postLostNew.addClass(this.mascota);
-                postLostNew.append('<div class="card shadow-sm"></div>');
-                postLostNew.append('<a href="/' + this.id + '"></a>');
-                postLostNew.find('a').append('<img src="/static/images/' + this.foto + '">');
                 let userInfo = $(document.createElement('div'));
                 userInfo.addClass('user');
-                userInfo.addClass('card-body');
-                userInfo.append('<h3>' + this.nombre + ' perdido/a!</h3>');
-                userInfo.append('<p class="card-text">Fecha de publicación: ' + this.created_at + '</p>');
+                userInfo.append('<h3>' + this.nombre +' perdido/a!</h3>');
+                userInfo.append('<p>Fecha de publicación: ' + this.created_at +'</p>');
                 postLostNew.append(userInfo);
                 postLostNew.append('<p>¡Se busca a ' + this.nombre + '! Perdido/a desde el día '
                 + this.fecha + ' última vez visto en las inmediaciones de ' + this.calle_1 +
                 ' y ' + this.calle_2 + ' barrio ' + this.barrio + ' a las ' + this.hora + ' horas.\n'
                 + 'Si lo viste por favor comunícate con ' + this.user_name + '.</p>');
+                postLostNew.append('<a href="/' + this.id + '"></a>');
+                postLostNew.find('a').append('<img src="/static/images/' + this.foto + '">');
                 let reportButton = $(document.createElement('button'));
                 reportButton.html('Denunciar publicación');
                 reportButton.addClass('btn');
@@ -42,28 +39,25 @@ $(document).ready(function () {
                 reportButton.addClass('btn-sm');
                 reportButton.attr('onclick', 'reportPost("' + this.id + '")');
                 postLostNew.append(reportButton);
-                $('div.posts').append(postLostNew);
+                $('article.posts').append(postLostNew);
             });
             $.each(data.found, function () {
                 this.created_at = new Date(this.created_at).toLocaleString('es-UY');
                 let postFoundNew = $(document.createElement('div'));
-                postFoundNew.addClass('col');
                 postFoundNew.addClass('pet');
                 postFoundNew.addClass('pet_found');
                 postFoundNew.addClass(this.mascota);
-                postFoundNew.append('<div class="card shadow-sm"></div>');
-                postFoundNew.append('<a href="/' + this.id + '"></a>');
-                postFoundNew.find('a').append('<img src="/static/images/' + this.foto + '">');
                 let userInfo = $(document.createElement('div'));
                 userInfo.addClass('user');
-                userInfo.addClass('card-body');
                 userInfo.append('<h3>' + this.mascota +' encontrado/a!</h3>');
-                userInfo.append('<p class="card-text">Fecha de publicación: ' + this.created_at + '</p>');
+                userInfo.append('<p>Fecha de publicación: ' + this.created_at +'</p>');
                 postFoundNew.append(userInfo);
                 postFoundNew.append('<p>Se encontró el día ' + this.fecha + ' por barrio '
                 + this.barrio + ' en las inmediaciones de ' + this.calle_1 +
                 ' y ' + this.calle_2 + ' a las ' + this.hora + ' horas.\n'
                 + 'Si es tuyo o sabes de quien puede ser por favor comunícate con ' + this.user_name + '.</p>');
+                postFoundNew.append('<a href="/' + this.id + '"></a>');
+                postFoundNew.find('a').append('<img src="/static/images/' + this.foto + '">');
                 let reportButton = $(document.createElement('button'));
                 reportButton.html('Denunciar publicación');
                 reportButton.addClass('btn');
@@ -71,35 +65,33 @@ $(document).ready(function () {
                 reportButton.addClass('btn-sm');
                 reportButton.attr('onclick', 'reportPost("' + this.id + '")');
                 postFoundNew.append(reportButton);
-                $('div.posts').append(postFoundNew);
+                $('article.posts').append(postFoundNew);
             });
         });
     }
     refreshFeed();
     window.refreshLost = function() {
         fetchAllPosts().then(function(data) {
-            $('div.posts').empty();
+            $('article.posts').empty();
             $('#todos').prop('checked', true);
             $.each(data.lost, function () {
                 this.created_at = new Date(this.created_at).toLocaleString('es-UY');
                 let postLostNew = $(document.createElement('div'));
-                postLostNew.addClass('col');
                 postLostNew.addClass('pet');
                 postLostNew.addClass('pet_lost');
                 postLostNew.addClass(this.mascota);
-                postLostNew.append('<div class="card shadow-sm"></div>');
-                postLostNew.append('<a href="/' + this.id + '"></a>');
-                postLostNew.find('a').append('<img src="/static/images/' + this.foto + '">');
                 let userInfo = $(document.createElement('div'));
                 userInfo.addClass('user');
-                userInfo.addClass('card-body');
-                userInfo.append('<h3>' + this.nombre + ' perdido/a!</h3>');
-                userInfo.append('<p class="card-text">Fecha de publicación: ' + this.created_at + '</p>');
+                userInfo.append('<h3>' + this.nombre +' perdido/a!</h3>');
+                userInfo.append('<p>Fecha de publicación: ' + this.created_at +'</p>');
                 postLostNew.append(userInfo);
                 postLostNew.append('<p>¡Se busca a ' + this.nombre + '! Perdido/a desde el día '
                 + this.fecha + ' última vez visto en las inmediaciones de ' + this.calle_1 +
                 ' y ' + this.calle_2 + ' barrio ' + this.barrio + ' a las ' + this.hora + ' horas.\n'
                 + 'Si lo viste por favor comunícate con ' + this.user_name + '.</p>');
+                postLostNew.append('<a href="/' + this.id + '"></a>');
+                postLostNew.find('a').append('<img src="static/images/' + this.foto + '">');
+                postLostNew.append('<a href="https://encuentrame.org.xelar.tech/report/' + this.id + '">Denunciar publicación</a>');
                 let reportButton = $(document.createElement('button'));
                 reportButton.html('Denunciar publicación');
                 reportButton.addClass('btn');
@@ -107,34 +99,32 @@ $(document).ready(function () {
                 reportButton.addClass('btn-sm');
                 reportButton.attr('onclick', 'reportPost("' + this.id + '")');
                 postLostNew.append(reportButton);
-                $('div.posts').append(postLostNew);
+                $('article.posts').append(postLostNew);
             });
         });
     }
     window.refreshFound = function() {
         fetchAllPosts().then(function(data) {
-            $('div.posts').empty();
+            $('article.posts').empty();
             $('#todos').prop('checked', true);
             $.each(data.found, function () {
                 this.created_at = new Date(this.created_at).toLocaleString('es-UY');
                 let postFoundNew = $(document.createElement('div'));
-                postFoundNew.addClass('col');
                 postFoundNew.addClass('pet');
                 postFoundNew.addClass('pet_found');
                 postFoundNew.addClass(this.mascota);
-                postFoundNew.append('<div class="card shadow-sm"></div>');
-                postFoundNew.append('<a href="/' + this.id + '"></a>');
-                postFoundNew.find('a').append('<img src="/static/images/' + this.foto + '">');
                 let userInfo = $(document.createElement('div'));
                 userInfo.addClass('user');
-                userInfo.addClass('card-body');
                 userInfo.append('<h3>' + this.mascota +' encontrado/a!</h3>');
-                userInfo.append('<p class="card-text">Fecha de publicación: ' + this.created_at + '</p>');
+                userInfo.append('<p>Fecha de publicación: ' + this.created_at +'</p>');
                 postFoundNew.append(userInfo);
                 postFoundNew.append('<p>Se encontró el día ' + this.fecha + ' por barrio '
                 + this.barrio + ' en las inmediaciones de ' + this.calle_1 +
                 ' y ' + this.calle_2 + ' a las ' + this.hora + ' horas.\n'
                 + 'Si es tuyo o sabes de quien puede ser por favor comunícate con ' + this.user_name + '.</p>');
+                postFoundNew.append('<a href="/' + this.id + '"></a>');
+                postFoundNew.find('a').append('<img src="static/images/' + this.foto + '">');
+                postFoundNew.append('<a href="https://encuentrame.org.xelar.tech/report/' + this.id + '">Denunciar publicación</a>');
                 let reportButton = $(document.createElement('button'));
                 reportButton.html('Denunciar publicación');
                 reportButton.addClass('btn');
@@ -142,7 +132,7 @@ $(document).ready(function () {
                 reportButton.addClass('btn-sm');
                 reportButton.attr('onclick', 'reportPost("' + this.id + '")');
                 postFoundNew.append(reportButton);
-                $('div.posts').append(postFoundNew);
+                $('article.posts').append(postFoundNew);
             });
         });
     }
