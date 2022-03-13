0e13452f083329ef4aeb8bfe8381dd22da6a6d99
Andy <3283@holbertonschool.com>
2022-03-13 03:56:19 -0300

    debugging user/pwd

--- app.py
+++ app.py
@@ -24,7 +24,10 @@ app.config['MYSQL_DB'] = 'encuentraMe'
 app.url_map.strict_slashes = False
 gmail_user = os.getenv('report_user')
 gmail_pwd = os.getenv('report_pwd')
-
+print(gmail_pwd)
+print(type(gmail_pwd))
+print(gmail_user)
+print(type(gmail_user))
 mysql = MySQL(app)
 
 
