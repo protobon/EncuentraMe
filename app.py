f50dce9e52afcda5a614680ae4c3422182d9dc59
ralexrivero <ralexrivero@gmail.com>
2022-03-14 05:29:32 -0300

    delete 404

--- app.py
+++ app.py
@@ -437,11 +437,6 @@ def reported_posts():
     return jsonify(reports)
 
 
-@app.errorhandler(404)
-def page_not_found(error):
-	return render_template('404.html'), 404
-
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
 else:
