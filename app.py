c255305dcd62c30a2cb1d6bf37d4e11c1c170627
ralexrivero <ralexrivero@gmail.com>
2022-03-14 05:24:16 -0300

    page not found and fix padding filter

--- app.py
+++ app.py
@@ -437,6 +437,11 @@ def reported_posts():
     return jsonify(reports)
 
 
+@app.errorhandler(404)
+def page_not_found(error):
+	return render_template('404.html'), 404
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
 else:
