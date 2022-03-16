5584c7310804fefc2426f3b0cf4695fb91ace4ba
ralexrivero <ralexrivero@gmail.com>
2022-03-16 11:29:48 -0300

    navigation tabs

---
+++ static/js/cookies.js
@@ -0,0 +1,19 @@
+window.cookieconsent.initialise({
+    "palette": {
+      "popup": {
+        "background": "#edeff5",
+        "text": "#838391"
+      },
+      "button": {
+        "background": "#4b81e8"
+      }
+    },
+    "type": "opt-out",
+    "content": {
+      "message": "Usamos cookies para que tengas la mejor experiencia",
+      "dismiss": "Ok",
+      "deny": "Cancelar",
+      "link": "Política de cookies",
+      "href": "politica-de-cookies.html"
+    }
+  });
