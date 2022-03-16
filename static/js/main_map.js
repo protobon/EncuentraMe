7a3297e2d7f0f00be1d947a8186aa6cfa3b72111
Andy <3283@holbertonschool.com>
2022-03-16 09:57:53 -0300

    Testing get position from a random element of list

--- static/js/main_map.js
+++ static/js/main_map.js
@@ -58,7 +58,7 @@ function initMap() {
       Object.assign(data[element], posit)
       listofallposts = listofallposts.concat(data[element])
     }
-    console.log(listofallposts)
+    console.log(listofallposts[2].position)
 
   })
       //Set icons
