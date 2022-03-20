fdca9c07e68822c0c217bac29d3800846a1b489b
ralexrivero <ralexrivero@gmail.com>
2022-03-20 04:30:34 -0300

    cookies

--- static/js/main.js
+++ static/js/main.js
@@ -2,3 +2,10 @@ const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-tog
 const tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl);
 });
+
+const myModal = document.getElementById('myModal');
+const myInput = document.getElementById('myInput');
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus();
+});
