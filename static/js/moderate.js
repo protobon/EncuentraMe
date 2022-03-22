ae316580f03e587c05419727f4c52b4d3284ead7
ralexrivero <ralexrivero@gmail.com>
2022-03-22 13:14:35 -0400

    card body

--- static/js/moderate.js
+++ static/js/moderate.js
@@ -8,19 +8,19 @@ $(document).ready(function () {
         fetchReports().then(function(data) {
             $.each(data.lost, function() {
                 let post = $(document.createElement('div'));
-                post.addClass('card row justify-content-around col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-4 p-2 g-2');
+                post.addClass('card justify-content-around col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-4 p-2 g-2');
                 post.addClass('pet');
                 post.addClass('pet-lost');
                 let cardImg = $(document.createElement('div'));/*2*/
                 cardImg.addClass('user'); /*2*/
                 cardImg.addClass('card-img-top');/*2*/
-                cardImg.append('<a href="https://encuentrame.org.xelar.tech/profile/' + this.user_id + '"></a>');
-                post.find('a').append('<h3>' + this.user_name + '</h3>');
                 post.append('<img class="img-fluid rounded" src="/static/images/' + this.foto + '">');
                 post.append('<p>' + this.created_at + '</p>');
                 $.each(this.comments, function() {
                     post.append('<p>' + this + '</p>');
                 });
+                let cardBody = $(document.createElement('div'));/*5*/
+                cardBody.addClass('card-body');/*5*/
                 let deleteButton = $(document.createElement('button'));
                 deleteButton.addClass('btn');
                 deleteButton.addClass('enc-btn-danger');
@@ -35,12 +35,13 @@ $(document).ready(function () {
                 forgiveButton.html('Perdonar');
                 forgiveButton.attr('onclick', 'forgivePost("' + this.id + '")');
                 post.append(cardImg);
+                post.append(cardBody);
                 post.append(forgiveButton);
                 $('div.posts').append(post);
             });
             $.each(data.found, function() {
                 let post = $(document.createElement('div'));
-                post.addClass('card row justify-content-around col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-4 p-2 g-2');
+                post.addClass('card justify-content-around col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-4 p-2 g-2');
                 post.addClass('pet');
                 post.addClass('pet-found');
                 let cardImg = $(document.createElement('div'));/*2*/
@@ -53,6 +54,8 @@ $(document).ready(function () {
                 $.each(this.comments, function() {
                     post.append('<p>' + this + '</p>');
                 });
+                let cardBody = $(document.createElement('div'));/*5*/
+                cardBody.addClass('card-body');/*5*/
                 let deleteButton = $(document.createElement('button'));
                 deleteButton.addClass('btn');
                 deleteButton.addClass('enc-btn-danger');
