217ee0cdc93bca008e3aa87370b6c7a920a8cef2
ralexrivero <ralexrivero@gmail.com>
2022-03-22 13:25:12 -0400

    bar

--- static/js/moderate.js
+++ static/js/moderate.js
@@ -8,65 +8,55 @@ $(document).ready(function () {
         fetchReports().then(function(data) {
             $.each(data.lost, function() {
                 let post = $(document.createElement('div'));
-                post.addClass('card justify-content-around col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-4 p-2 g-2');
                 post.addClass('pet');
                 post.addClass('pet-lost');
-                let cardImg = $(document.createElement('div'));/*2*/
-                cardImg.addClass('user'); /*2*/
-                cardImg.addClass('card-img-top');/*2*/
-                cardImg.append('<a href="https://encuentrame.org.xelar.tech/profile/' + this.user_id + '"></a>');
+                post.append('<a href="https://encuentrame.org.xelar.tech/profile/' + this.user_id + '"></a>');
                 post.find('a').append('<h3>' + this.user_name + '</h3>');
-                post.append('<img class="img-fluid rounded" src="/static/images/' + this.foto + '">');
+                post.append(';<img src="/static/images/' + this.foto + '">');
                 post.append('<p>' + this.created_at + '</p>');
                 $.each(this.comments, function() {
                     post.append('<p>' + this + '</p>');
                 });
                 let deleteButton = $(document.createElement('button'));
                 deleteButton.addClass('btn');
-                deleteButton.addClass('enc-btn-danger');
+                deleteButton.addClass('btn-danger');
                 deleteButton.attr('type', 'button');
                 deleteButton.html('Eliminar');
                 deleteButton.attr('onclick', 'deletePost("' + this.id + '")');
                 post.append(deleteButton);
                 let forgiveButton = $(document.createElement('button'));
                 forgiveButton.addClass('btn');
-                forgiveButton.addClass('enc-btn-happy');
+                forgiveButton.addClass('btn-primary');
                 forgiveButton.attr('type', 'button');
                 forgiveButton.html('Perdonar');
                 forgiveButton.attr('onclick', 'forgivePost("' + this.id + '")');
-                post.append(cardImg);
                 post.append(forgiveButton);
                 $('div.posts').append(post);
             });
             $.each(data.found, function() {
                 let post = $(document.createElement('div'));
-                post.addClass('card justify-content-around col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-4 p-2 g-2');
                 post.addClass('pet');
                 post.addClass('pet-found');
-                let cardImg = $(document.createElement('div'));/*2*/
-                cardImg.addClass('user'); /*2*/
-                cardImg.addClass('card-img-top');/*2*/
-                cardImg.append('<img class="img-fluid rounded" src="/static/images/' + this.foto + '">');
                 post.append('<h3>' + this.user_name + '</h3>');
                 post.append('<p>' + this.user_id + '</p>');
+                post.append('<img src="/static/images/' + this.foto + '">');
                 post.append('<p>' + this.created_at + '</p>');
                 $.each(this.comments, function() {
                     post.append('<p>' + this + '</p>');
                 });
                 let deleteButton = $(document.createElement('button'));
                 deleteButton.addClass('btn');
-                deleteButton.addClass('enc-btn-danger');
+                deleteButton.addClass('btn-danger');
                 deleteButton.attr('type', 'button');
                 deleteButton.html('Eliminar');
                 deleteButton.attr('onclick', 'deletePost("' + this.id + '")');
                 post.append(deleteButton);
                 let forgiveButton = $(document.createElement('button'));
                 forgiveButton.addClass('btn');
-                forgiveButton.addClass('enc-btn-happy');
+                forgiveButton.addClass('btn-primary');
                 forgiveButton.attr('type', 'button');
                 forgiveButton.html('Perdonar');
                 forgiveButton.attr('onclick', 'forgivePost("' + this.id + '")');
-                post.append(cardImg);
                 post.append(forgiveButton);
                 $('div.posts').append(post);
             });
